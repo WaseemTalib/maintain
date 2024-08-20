f4010bd38b70085d9b7f9a0b31f942a15db4a800
MuhammadHamzaWaqar1317 <waqarhamza2003@gmail.com>
2024-08-20 23:43:26 +0500

    feat: maintain space

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import "./App.css";
 function App() {
   return (
     <>
-      {" "}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite" />
